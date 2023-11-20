1010cb313ff93a5e731afefa83d0f372ec63f649
batsalay-illya <125219597+batsalay-illya@users.noreply.github.com>
2023-11-20 16:03:56 +0300

    Update main.scala

--- Skillonomy/main.scala
+++ Skillonomy/main.scala
@@ -7,7 +7,7 @@ def main(): Unit = {
 def Simulation(period: Int): Unit ={
   DataBase.SimulateRegistration(10)
 
-  DataBase.ListOfTeachers.list.foreach(teacher => teacher.CreateCourse(DataBase.GenerateRandomString(), Random(6, 12), if(math.random < 0.5) 500 else 1000, 0.1))
+  DataBase.ListOfTeachers.list.foreach(teacher => teacher.CreateCourse(DataBase.GenerateRandomString(), Random.between(6, 12), if(math.random < 0.5) 500 else 1000, 0.1))
   DataBase.ListOfTeachers.list.foreach(teacher => teacher.CreateOffice(new Office(id = DataBase.ListOfOffices.list.length)))
 
   for(n <- 1 to period) {
